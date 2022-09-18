e20839e1932ed8ce88c346d4f9ffea6be814639d
Gelliona <Gelliona81@mail.ru>
2022-09-18 22:21:42 +0400

    TASK_4 added

---
+++ TASK_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+
+int maximum = number1;
+
+if (number2 > maximum)
+{
+    maximum = number2;
+}
+if (number3 > maximum)
+{
+    maximum = number3;
+}
+
+Console.WriteLine(maximum);
