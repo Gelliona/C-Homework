0bfd29f8cdc6568e8024b5f8836dbc9387303df5
Gelliona <Gelliona81@mail.ru>
2022-09-18 23:02:42 +0400

    TASK_8 added

---
+++ TASK_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
