1acf733d8d4f916d01d93f13dfe1fcf56c8cab37
Gelliona <Gelliona81@mail.ru>
2022-09-21 21:26:39 +0400

    DAY 2. TASK_15 added

---
+++ TASK_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+if ((number >= 1) && (number <= 7))
+{
+    if ((number == 6) || (number == 7))
+        Console.WriteLine("Это выходной день");
+    else
+        Console.WriteLine("Это рабочий день");
+}
+else
+    Console.WriteLine("Ошибка ввода! Введите число от 1 до 7");
