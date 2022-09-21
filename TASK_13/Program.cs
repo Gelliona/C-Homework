dd321b9c50119cdb561550209f1c84a4d5a69c60
Gelliona <Gelliona81@mail.ru>
2022-09-21 21:15:54 +0400

    DAY 2. TASK_13 added

---
+++ TASK_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100)
+{
+    while (number >= 999)
+        number = number / 10;
+    number = number % 10;
+    Console.WriteLine(number);
+}
+else
+    Console.WriteLine("В числе нет третьей цифры");
+
