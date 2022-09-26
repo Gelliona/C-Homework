fce9ccb36e09e7feca65f33d179db0b324389143
Gelliona <Gelliona81@mail.ru>
2022-09-26 09:25:25 +0400

    DAY 3. TASK_23 added

---
+++ TASK_23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на вход число N и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+for (int count = 1; count <= num; count++)
+{
+    Console.Write($"{count*count*count}");
+    if (count!=num)
+        Console.Write($", ");
+}
+
+
