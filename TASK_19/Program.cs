1deba1a8851ab765c3ccf4d97326368af41f342e
Gelliona <Gelliona81@mail.ru>
2022-09-26 09:10:53 +0400

    DAY 3. TASK_19 added

---
+++ TASK_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int Digit1 = number/10000;
+int Digit2 = number%10000/1000;
+int Digit4 = number%100/10;
+int Digit5 = number%10;
+
+bool Palindrom = false;
+
+if (Digit1==Digit5)
+{
+    if (Digit2==Digit4)
+    {
+        Palindrom = true;
+    }
+}
+
+
+if (Palindrom == true)
+{
+    Console.WriteLine("Данное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Данное число не является палиндромом");
+}    
+
