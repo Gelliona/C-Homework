bc1c9fed381e1e99ac784a98eb8e9d95394958ee
Gelliona <Gelliona81@mail.ru>
2022-09-26 10:23:11 +0400

    DAY 3. TASK_21 added

---
+++ TASK_21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты точки А: ");
+Console.Write("x= ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("y= ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.Write("z= ");
+int z1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("x= ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("y= ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.Write("z= ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+int dx = x2 - x1;
+int dy = y2 - y1;
+int dz = z2 - z1;
+
+double dist = Math.Pow(dx * dx + dy * dy + dz * dz, 0.5);
+
+Console.WriteLine($"Расстояние между точками = {dist:f2}");
+
+
