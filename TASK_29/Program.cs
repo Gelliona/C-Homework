381649953f0d604385d84a9c6fbc66cda4d88386
Gelliona <Gelliona81@mail.ru>
2022-09-30 18:08:12 +0400

    DAY 4. TASK_29 added

---
+++ TASK_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next();
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        Console.Write($", ");
+    }
+    Console.Write("]");
+}
+
+
+
+
