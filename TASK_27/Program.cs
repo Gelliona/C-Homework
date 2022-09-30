226ce58c3a45b6708f6d352e8444dd776ec0a29e
Gelliona <Gelliona81@mail.ru>
2022-09-30 18:31:24 +0400

    DAY 4. TASK_27 added

---
+++ TASK_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int summa = 0;
+
+int SummaOfDigits(int num)
+{
+    while (num != 0)
+    {
+        int lastDigit = num % 10;
+        summa = summa + lastDigit;
+        num = num / 10;
+    }
+    if (summa < 0)
+        summa = summa * (-1);
+    return summa;
+}
+
+int total = SummaOfDigits(number);
+Console.WriteLine($"Сумма цифр в этом числе = {total}");
+
