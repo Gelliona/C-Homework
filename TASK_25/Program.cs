524e5b43a122053cc89eb1e59420e3544fd54f28
Gelliona <Gelliona81@mail.ru>
2022-09-30 19:35:25 +0400

    DAY 4. TASK_25 added

---
+++ TASK_25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Цикл принимает на вход два числа (А и В) и возводит А в натуральную степень В
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+
+int Power(int num1, int num2)
+{
+    int total = 1;
+    while (num2 != 0)
+    {
+        total = total * num1;
+        num2--;
+    }
+    return total;
+}
+
+int total = Power(a, b);
+
+Console.WriteLine($"{a} ^ {b} = {total}");
+
