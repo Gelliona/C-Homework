9031def6bb4a9f260a30a24dce25d1d943a07d88
Gelliona <Gelliona81@mail.ru>
2022-10-02 19:42:38 +0400

    DAY 5. TASK_34 added

---
+++ TASK_34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Программа заполняет массив случайными положительными трехзначными числами и показывает количество четных чисел в массиве
+
+Console.Clear();
+
+int[] array = CreateArray();
+FillArray(array);
+PrintArray(array);
+int evenNumbers = sumOfEven(array);
+Console.WriteLine();
+Console.WriteLine($"В массиве {evenNumbers} четных элементов");
+
+
+int[] CreateArray()
+{
+    int[] array = new int[new Random().Next(10, 21)];
+    return array;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(100, 1000);
+    }
+}
+
+int sumOfEven(int[] arr)
+{
+    int counter = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]%2 == 0)
+            counter++;
+    }
+    return counter;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        Console.Write($", ");
+    }
+    Console.Write("]");
+}
