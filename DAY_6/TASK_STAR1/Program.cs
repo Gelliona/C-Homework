0a6a7769a0c9ffe37b0b99d216832616e8dc9cd5
Gelliona <Gelliona81@mail.ru>
2022-10-07 23:30:27 +0400

    DAY_6. TASK_STAR1 added

---
+++ DAY_6/TASK_STAR1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа рекурсивным методом переводит числа из десятичной системы в двоичную
+
+Console.Clear();
+
+Console.Write("Введите десятичное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int binaryNumber = int.Parse (BinaryConverting(number));
+Console.WriteLine(binaryNumber);
+
+
+string BinaryConverting(int num, string mod = "", string binNum = "")
+{
+    if (num < 2)
+    {
+        binNum = 1 + binNum;
+        return (binNum);
+    }
+    else
+    {
+        //num = num / 2;
+        mod = Convert.ToString(num % 2);
+        binNum = mod + binNum ;
+        return BinaryConverting(num/2, mod = "", binNum);
+    }
+  
+}
