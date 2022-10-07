c80b6cc6fd1fd996e5a4a41bea281682d583a64e
Gelliona <Gelliona81@mail.ru>
2022-10-07 09:48:32 +0400

    DAY_6. TASK_41 added

---
+++ DAY_6/TASK_41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Программа принимает число A. После этого пользователь вводит А чисел. Программа выводит, сколько из них было положительных.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int pozitiveCount = PositiveNumbers(num);
+Console.WriteLine(pozitiveCount);
+
+/*
+int PositiveNumbers(int number)                                 // Вариант с классическим циклом
+{
+    int counter = 0;
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write("Введите число: ");
+        int newNumber = int.Parse(Console.ReadLine()!);
+        if (newNumber > 0)
+            counter++;
+    }
+    return counter;
+}
+*/
+
+int PositiveNumbers(int number, int i = 1, int counter = 0)     // Вариант с рекурсией
+{
+    if (i > number) 
+        return counter;
+    else
+    {
+        int newNumber = int.Parse(Console.ReadLine()!);
+        if (newNumber > 0)
+        {
+            return PositiveNumbers(number, ++i, ++counter);
+        }
+        else
+        {
+            return PositiveNumbers(number, ++i, counter);
+        }    
+    }
+}
+
+
