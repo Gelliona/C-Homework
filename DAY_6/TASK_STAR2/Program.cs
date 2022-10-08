7fd9460f2f1e0264d71553a843c612b81b36f4cc
Gelliona <Gelliona81@mail.ru>
2022-10-08 17:31:29 +0400

    DAY_6.TASK_STAR2 added

---
+++ DAY_6/TASK_STAR2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принимает от пользователя положительные числа, пока не будет введено "-1", и выдаёт среднее арифметическое введённых чисел
+
+Console.Clear();
+
+double averageNum = Average();
+Console.WriteLine(averageNum);
+
+
+
+
+double Average(double summa = 0, int counter = 0)     
+{
+    Console.Write("Введите число: ");
+    double number = double.Parse(Console.ReadLine()!);
+
+    if (number == -1) 
+        return summa / counter;
+    else
+    {
+        return Average(summa+number, ++counter);   
+    }
+}
