ad81b3a61c2e7bc03cfdaf7f1ea2a5af20462220
Gelliona <Gelliona81@mail.ru>
2022-10-13 11:40:01 +0400

    DAY_7. TASK_47 added

---
+++ DAY_7/TASK_47/Program.cs
@@ -0,0 +1,39 @@
+﻿//Программа выводит двумерный массив m x n, заполненный случайными вещественными числами
+
+Console.Clear();
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+double[,] array = new double[m, n];
+FillArray(array);
+PrintArray(array);
+
+
+void PrintArray(double[,] array)
+{
+    for (int i=0; i < array.GetLength(0); i++)
+    {
+        for (int j=0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArray(double[,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(generator.Next(-100,100)/10.0,1);
+        }
+    }
+}
