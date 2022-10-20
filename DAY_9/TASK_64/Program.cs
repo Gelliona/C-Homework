4720648e2c275fa1e1af7db6a3f04804f26c0f03
Gelliona <Gelliona81@mail.ru>
2022-10-20 08:53:40 +0400

    DAY_9. TASK_64 added

---
+++ DAY_9/TASK_64/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое положительное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+NaturalNumbersSequence(number);
+
+
+
+void NaturalNumbersSequence(int num)
+{
+    if (num == 1)
+        Console.Write($"{num}");    
+    else
+    {
+        Console.Write($"{num}, ");
+        NaturalNumbersSequence(num-1);
+    }
+}
