f4684f7c4c01c9c333bc7539e13334df108658ca
Gelliona <Gelliona81@mail.ru>
2022-10-20 09:51:12 +0400

    DAY_9. TASK_68 added

---
+++ DAY_9/TASK_68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа вычисляет функцию Аккермана с помощью рекурсии.
+
+Console.Clear();
+
+Console.Write("Введите первое положительное число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе положительное число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+int result = Akkerman(firstNumber, secondNumber);
+Console.WriteLine(result);
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;   
+    else if (m > 0 && n == 0)
+        return Akkerman(m-1, 1); 
+    else 
+    {
+        return Akkerman(m-1, Akkerman(m, n-1));
+    }
+}
