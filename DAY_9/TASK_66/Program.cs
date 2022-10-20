cea7161a16f09ca8301c98ef054eb92276880484
Gelliona <Gelliona81@mail.ru>
2022-10-20 10:36:32 +0400

    DAY_9. TASK_66 added

---
+++ DAY_9/TASK_66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа находит сумму натуральных элементов в промежутке от M до N рекурсивно.
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+int summa = RecSumma(firstNumber, secondNumber);
+Console.WriteLine(summa);
+
+
+
+int RecSumma(int a, int b, int summa = 0)
+{
+    if (a > b)
+        return summa;    
+    else
+    {
+        return RecSumma(a + 1, b, summa + a);
+    }
+}
